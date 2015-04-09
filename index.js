55be538130c3ee8060dc980fc485e2b6373e9bf3
Dominic Michael Joshua Dunnett <domdunnett@gmail.com>
2015-04-09 20:34:58 +0800

    add routes.js

--- index.js
+++ index.js
@@ -2,16 +2,21 @@ var Hapi = require('hapi');
 var server = new Hapi.Server();
 
 server.connection({
-	host: 'localhost',
-	port: 8000
+	host: '0.0.0.0',
+	port: 8080,
+  routes: {
+    cors: true
+  }
 });
 
-server.route({
-	method: 'GET',
-	path: '/hello',
-	handler: function(request, reply) {
-		reply("Hello World");
-	}
+var plugins = [{ register: require('./routes/quotes.js') }];
+
+server.register(plugins, function (err) {
+  if (err) { throw err; }
+
+  server.start(function () {
+    server.log('info', 'Server running at: ' + server.info.uri);
+  });
 });
 
 server.start();
